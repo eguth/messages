609eff85b85430188dd565ed40aa549569220bf5
Steven Davidovitz <sdavidovitz@zendesk.com>
2013-07-22 21:02:05 -0700

    add a faye.js

--- public/application.js
+++ public/application.js
@@ -9,40 +9,6 @@ $(document).ready(function() {
   var replying = $("#message #replying");
   var preview_close = $("#preview-area .close");
 
-  // Handles Message pub-sub
-  if(typeof Settings != 'undefined') {
-    var client = new Faye.Client('//' + window.location.host + '/faye');
-    var subscription = client.subscribe(Settings.current_channel, function(message) {
-      var messages = $("#messages");
-
-      message = JSON.parse(message);
-
-      var parent_message = $("#message-" + message.parent_id);
-
-      if(message.parent_id && parent_message.length > 0) {
-        var children = parent_message.find(".children");
-        children.append(message.body);
-
-        var num_children = children.children(".message-child").length;
-        parent_message.find(".toggle-children > span").html(num_children);
-
-        replying.children(".close").click();
-
-        children.show();
-        parent_message.effect("highlight", { color: "#8aaf19" });
-      } else {
-        messages.prepend(message.body)
-
-        var children = messages.children(".message");
-        children.first().effect("highlight", { color: "#8aaf19" });
-
-        if(children.length > Settings.max_messages) {
-          children.last().remove();
-        }
-      };
-    });
-  }
-
   $(".toggle-children").on('click', function(event) {
     event.preventDefault();
 
